5eeae32181301ee02ac60447e051b5e60fcfd965
Harini Shri <148843591+Harini-Shri-03@users.noreply.github.com>
2026-05-28 22:28:18 +0100

    Added small-world generator skeleton file

--- synthetic_network/small_world_generator.m
+++ synthetic_network/small_world_generator.m
@@ -1,1 +1,9 @@
+% Small-World Network Generator
+% Weeks 3-4 Planning Phase
 
+% Parameters
+n = 50;
+k = 4;
+p = 0.1;
+
+disp('Small-world network implementation will be completed in next phase.');

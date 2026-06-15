159a9b58b0cdf347cf103876bd6e3c4bfa447ca5
Harini Shri <148843591+Harini-Shri-03@users.noreply.github.com>
2026-06-15 21:40:23 +0100

    run_baseline.m

--- sir_model/run_baseline.m
+++ sir_model/run_baseline.m
@@ -1,1 +1,11 @@
+A = small_world_generator(100,4,0.1);
 
+beta = 0.3;
+gamma = 0.1;
+
+[S,I,R] = sir_model(A, beta, gamma, 1, 50);
+
+plot(I);
+title('Baseline Infection Curve');
+xlabel('Time');
+ylabel('Infected Nodes');

b648d2d9da5acd9525062c61d7d6208b45786c36
Harini Shri <148843591+Harini-Shri-03@users.noreply.github.com>
2026-07-14 22:31:26 +0100

    Update monte_carlo_simulation.m

--- sir_model/monte_carlo_simulation.m
+++ sir_model/monte_carlo_simulation.m
@@ -1,6 +1,16 @@
-Run SIR multiple times (e.g., 50 runs)
-Store peak infection each run
-Compute:
-mean()
-std()
-confidence interval
+function [avg_peak, CI_low, CI_high] = monte_carlo(G,beta,gamma,numMC)
+
+peaks = zeros(numMC,1);
+
+for i=1:numMC
+    peaks(i) = sir_simulation(G,beta,gamma);
+end
+
+avg_peak = mean(peaks);
+std_peak = std(peaks);
+
+margin = 1.96*(std_peak/sqrt(numMC));
+CI_low = avg_peak - margin;
+CI_high = avg_peak + margin;
+
+end

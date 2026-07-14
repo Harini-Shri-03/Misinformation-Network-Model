acd7320601dcf9c7082f1b19836415865e668949
Harini Shri <148843591+Harini-Shri-03@users.noreply.github.com>
2026-07-14 22:29:47 +0100

    Update small_world_generator.m

--- synthetic_network/small_world_generator.m
+++ synthetic_network/small_world_generator.m
@@ -1,11 +1,31 @@
-% Small-World Network Generator
-% Weeks 3-4 Planning Phase
+function G = small_world_generator(N,K,p)
 
-% Parameters
-n = 50;
-k = 4;
-p = 0.1;
+A = zeros(N);
 
-G = graph();
+% Ring lattice
+for i = 1:N
+    for j = 1:K
+        right = mod(i-1 + j, N) + 1;
+        left  = mod(i-1 - j, N) + 1;
 
-disp('Small-world network implementation will be completed in next phase.');
+        A(i,right)=1; A(right,i)=1;
+        A(i,left)=1;  A(left,i)=1;
+    end
+end
+
+% Rewiring
+for i = 1:N
+    for j = 1:K
+        neighbor = mod(i-1 + j, N) + 1;
+        if rand < p
+            A(i,neighbor)=0; A(neighbor,i)=0;
+            possible = setdiff(1:N,[i find(A(i,:))]);
+            new_node = possible(randi(length(possible)));
+            A(i,new_node)=1; A(new_node,i)=1;
+        end
+    end
+end
+
+G = graph(A);
+
+end
